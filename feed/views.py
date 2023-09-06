43867927787dca145cc68c061f63b37697d31166
SanishKumar <18sanishkumar@gmail.com>
2023-09-06 17:09:01 +0530

    File Uploaded!

---
+++ feed/views.py
@@ -0,0 +1,11 @@
+from django.views.generic import ListView
+
+from .models import Post
+
+
+class HomePage (ListView):
+    http_method_names = ["get"]
+    template_name = "feed/homepage.html"
+    model = Post
+    context_object_name = "posts"
+    queryset = Post.objects.all().order_by('-id')[0:30]
